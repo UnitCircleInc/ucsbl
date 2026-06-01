9e1b2ec62edfd85c1af4f627c8d8def7d081b9ff
Sean Simmons <sean@unitcircle.ca>
2026-06-01 12:42:45 -0400

    fix copyright year

--- prod-sbl/main.c
+++ prod-sbl/main.c
@@ -1,4 +1,4 @@
-// © 2025 Unit Circle Inc.
+// © 2026 Unit Circle Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
